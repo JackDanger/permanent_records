38ae0b752f77a6f0e248cbed0cca4849665a340e
Elijah Miller <elijah.miller@gmail.com>
2009-11-13 11:23:18 -0500

    Forcing table creation so tests on persistent databases can run.

--- test/schema.rb
+++ test/schema.rb
@@ -1,21 +1,21 @@
 ActiveRecord::Schema.define(:version => 1) do
   
-  create_table :muskrats do |t|
+  create_table :muskrats, :force => true do |t|
     t.column :name,         :string
     t.column :deleted_at,   :datetime
     t.references :hole
   end
   
-  create_table :kitties do |t|
+  create_table :kitties, :force => true do |t|
     t.column :name,   :string
   end
 
-  create_table :holes do |t|
+  create_table :holes, :force => true do |t|
     t.integer :number
     t.datetime :deleted_at
   end
   
-  create_table :moles do |t|
+  create_table :moles, :force => true do |t|
     t.string :name
     t.references :hole
   end

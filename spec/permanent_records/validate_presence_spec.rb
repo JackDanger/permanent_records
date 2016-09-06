fe2079bae6aeb968c459a2f96bf6a688f3b2cdb1
Joel AZEMAR <joel.azemar@gmail.com>
2016-09-06 16:22:34 +0200

    Add case of revival children first

--- spec/permanent_records/validate_presence_spec.rb
+++ spec/permanent_records/validate_presence_spec.rb
@@ -23,6 +23,19 @@ describe PermanentRecords do
           room.reload.deleted?
         }.to(false)
       end
+
+      it 'it\'s impossible to revive children first when child validate presence of parent and have a default scope' do
+        expect {
+          house.destroy
+        }.to change {
+          house.reload.deleted?
+        }.to(true) & change {
+          room.reload.deleted?
+        }.to(true)
+
+        # Room Validation failed
+        expect { house.revive }.to raise_error(ActiveRecord::RecordInvalid, "Validation failed: House can't be blank")
+      end
     end
   end
 end

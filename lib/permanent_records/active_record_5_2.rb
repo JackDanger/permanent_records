a46dbf0d46d16f53659a47e692e557c6c26d309d
msalzburg <salzburg@emjot.de>
2019-10-15 12:27:15 +0200

    Introduce rails 5.2 fix from Nancy Cai PR #100

---
+++ lib/permanent_records/active_record_5_2.rb
@@ -0,0 +1,33 @@
+module HandlePermanentRecordsDestroyedInBelongsToAssociation
+  def handle_dependency
+    return unless load_target
+
+    case options[:dependent]
+    when :destroy
+      target.destroy
+      raise ActiveRecord::Rollback if target.respond_to?(:deleted?) && !target.deleted?
+    else
+      target.send(options[:dependent])
+    end
+  end
+end
+
+module HandlePermanentRecordsDestroyedInHasOneAssociation
+  def delete(method = options[:dependent])
+    return unless load_target
+
+    case method
+    when :delete
+      target.delete
+    when :destroy
+      target.destroyed_by_association = reflection
+      target.destroy
+      throw(:abort) if target.respond_to?(:deleted?) && !target.deleted?
+    when :nullify
+      target.update_columns(reflection.foreign_key => nil) if target.persisted?
+    end
+  end
+end
+
+ActiveRecord::Associations::BelongsToAssociation.prepend(HandlePermanentRecordsDestroyedInBelongsToAssociation)
+ActiveRecord::Associations::HasOneAssociation.prepend(HandlePermanentRecordsDestroyedInHasOneAssociation)
